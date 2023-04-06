44e313d9e89ad84457234596490781917eef5abc
Ercan Yigit <ercanyter@gmail.com>
2023-04-07 00:43:36 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/demirbasim/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.demirbasim
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/home_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class DemirBasimHome extends StatefulWidget {
+  const DemirBasimHome({super.key});
+
+  @override
+  State<DemirBasimHome> createState() => _DemirBasimHomeState();
+}
+
+class _DemirBasimHomeState extends State<DemirBasimHome> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
